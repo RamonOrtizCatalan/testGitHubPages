b498f38860669ad668459a2d9a715ce91ce094e3
Ramon Ortiz Catalan <ramonortizcatalan@gmail.com>
2024-03-06 10:04:32 -0800

    navigationmodified

--- navigation.js
+++ navigation.js
@@ -5,7 +5,7 @@ document.write('\
    <li><a href="/testGitHubPages/index.html">Home</a></li>\
    <li><a href="/testGitHubPages/collaborators.html">Collaborators</a></li>\
    <li><a href="/testGitHubPages/publications.html">Publications</a></li>\
-   <li><a href="https://github.com/OpenTOPAS/topas" target="_blank">Download</a></li>\
+   <li><a href="/testGitHubPages/download.html" target="_blank">Download</a></li>\
    <li><a href="https://drive.google.com/file/d/1S87uBvhNwNmLABKiceuw65X3RP1P9RrZ/view" target="_blank">Installation guide</a></li>\
    <li><a href="https://topas.readthedocs.io/en/latest/" target="_blank">User guide</a></li>\
    <li><a href="https://github.com/OpenTOPAS/topas/discussions" target="_blank">User forum</a></li>\
