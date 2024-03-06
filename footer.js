c9bd10ea2a0b4afae4f3f256a6b8cbc0d95ed619
Ramon Ortiz Catalan <ramonortizcatalan@gmail.com>
2024-03-05 23:09:21 -0800

    update

---
+++ footer.js
@@ -0,0 +1,6 @@
+document.write('\
+\
+     <footer>2024. Open TOPAS official site. Last update: March 5, 2024.\
+    </footer>\
+\
+');
---
+++ navigation.js
@@ -0,0 +1,18 @@
+document.write('\
+\
+<nav>\
+<ul>\
+   <li><a href="/testGitHubPages/index.html">Home</a></li>\
+   <li><a href="/testGitHubPages/collaborators.html">Collaborators</a></li>\
+   <li><a href="#">Publications</a></li>\
+   <li><a href="#">Download</a></li>\
+   <li><a href="#">Installation guide</a></li>\
+   <li><a href="https://topas.readthedocs.io/en/latest/" target="_blank">User guide</a></li>\
+   <li><a href="#">External tools</a></li>\
+   <li><a href="#">Lincese</a></li>\
+   <li><a href="#">Contact</a></li>\
+   <li><a href="#">User forum</a></li>\
+</ul>\
+</nav>\
+\
+');
