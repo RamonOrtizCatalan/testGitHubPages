1d85b122aea9fe01e72778b150d8ef4d15fac149
Ramon Ortiz Catalan <ramonortizcatalan@gmail.com>
2024-03-06 10:01:23 -0800

    fullversion

--- navigation.js
+++ navigation.js
@@ -4,14 +4,14 @@ document.write('\
 <ul>\
    <li><a href="/testGitHubPages/index.html">Home</a></li>\
    <li><a href="/testGitHubPages/collaborators.html">Collaborators</a></li>\
-   <li><a href="#">Publications</a></li>\
-   <li><a href="#">Download</a></li>\
-   <li><a href="#">Installation guide</a></li>\
+   <li><a href="/testGitHubPages/publications.html">Publications</a></li>\
+   <li><a href="https://github.com/OpenTOPAS/topas" target="_blank">Download</a></li>\
+   <li><a href="https://drive.google.com/file/d/1S87uBvhNwNmLABKiceuw65X3RP1P9RrZ/view" target="_blank">Installation guide</a></li>\
    <li><a href="https://topas.readthedocs.io/en/latest/" target="_blank">User guide</a></li>\
+   <li><a href="https://github.com/OpenTOPAS/topas/discussions" target="_blank">User forum</a></li>\
    <li><a href="#">External tools</a></li>\
-   <li><a href="#">Lincese</a></li>\
+   <li><a href="/testGitHubPages/news.html">News</a></li>\
    <li><a href="#">Contact</a></li>\
-   <li><a href="#">User forum</a></li>\
 </ul>\
 </nav>\
 \
